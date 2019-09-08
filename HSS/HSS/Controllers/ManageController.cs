c03d5fdb46e1e90fefd091163a2414b11e125eb9
Kacper <pasnik.kacper@gmail.com>
2019-09-08 02:24:40 +0200

    Added an option to Edit rows in manage section.

--- HSS/HSS/Controllers/ManageController.cs
+++ HSS/HSS/Controllers/ManageController.cs
@@ -20,16 +20,14 @@ namespace HSS.Controllers
             var all = db.Customer.ToList();
 
             if (options == "1")
-                all = db.Customer.Where(x => x.CustomerId.ToString() == search || search == null).ToList();
-            else if (options == "2")
                 all = db.Customer.Where(x => x.Name.StartsWith(search) || search == null).ToList();
-            else if (options == "3")
+            else if (options == "2")
                 all = db.Customer.Where(x => x.Surname.StartsWith(search) || search == null).ToList();
-            else if (options == "4")
+            else if (options == "3")
                 all = db.Customer.Where(x => x.Date_from.ToString() == search || search == null).ToList();
-            else if (options == "5")
+            else if (options == "4")
                 all = db.Customer.Where(x => x.Date_to.ToString() == search || search == null).ToList();
-            else if (options == "6")
+            else if (options == "5")
                 all = db.Customer.Where(x => x.Room.ToString() == search || search == null).ToList();
 
             ManageViewModel vm = new ManageViewModel()
@@ -38,6 +36,33 @@ namespace HSS.Controllers
             };
 
             return View(vm);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var customer= db.Customer.Single(x => x.CustomerId == id);
+            return View(customer);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Customer customer, int id)
+        {
+
+            if (ModelState.IsValid)
+            {
+                HSSContext db = new HSSContext();
+                var x = db.Customer.Single(u => u.CustomerId == id);
+                x.Name = customer.Name;
+                x.Surname = customer.Surname;
+                x.Date_from = customer.Date_from;
+                x.Date_to = customer.Date_to;
+                x.Room = customer.Room;
+                db.SaveChanges();
+                return RedirectToAction("Manage");
+                //return View("Manage");
+            }
+            return View(customer);
         }
 
     }
